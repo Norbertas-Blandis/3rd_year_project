2e36a7f0bf3808c8aa93a10fd06e53b1f395972a
Norbertas Blandis <norbertas.blandis@student.manchester.ac.uk>
2022-10-01 15:41:06 +0100

    Initial Commit

---
+++ firstTest/firstTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTestApp.swift
+//  firstTest
+//
+//  Created by Norbertas Blandis on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct firstTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
